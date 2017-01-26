b2cf4e52f99c6804950e4a7cae12d2817c1cef9b
Yomi <alajide@gmail.com>
2017-01-26 17:24:42 -0500

    more vanishy stuff

--- javascripts/board.js
+++ javascripts/board.js
@@ -1,16 +1,41 @@
 class Board {
   constructor(){
     this.positions = ["NO", "YES"]
-    this.stufftoShow = ["image1", "image2", "image3", "image4","image5"] // each is an image object???
+    this.stuffToShow = ["images/octagon-512.png", "images/octagon-512.png", "images/octagon-512.png", "images/octagon-512.png","images/octagon-512.png"] // each is an image object???
   }
 
+  pictureCycler(){
+    var promise = $.when();
+    var self = this;
+    for(let i = 0; i < this.stuffToShow.length; i++){
+      promise = promise.then(function(element){
+        if(element){
+          $(`#${i-1}`).fadeOut(1)
+        }
+        return $('#picture').append(self.renderImg(self.stuffToShow[i], i))
+          .delay(2000)
+          .promise();
+      })
+    }
+  }
 
-pictureCycler(){
-  $("#picture").delay(1000).fadeOut(100)
+  // pictureCycler() {
+  //   // window.setTimeout(function() {
+  //   //   this.stufftoShow.forEach((image, index)=>{
+  //   //   })
+  //   // var img = this.stufftoShow[0]
+  //   // var self = this;
+  //   this.stufftoShow.forEach((image,index) => {
+  //     window.setTimeout(this.renderImg(image, 0), 10000)
+  // })
+// }
+   renderImg(image, index){
+     $("#picture").append(`<div id=${index}><img src=${image}></div>`)
+   }
 }
+
+
 
   // render(){
   //   let $board = $('#board')
   // }
-
-}
